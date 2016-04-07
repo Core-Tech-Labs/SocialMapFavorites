69cbba232ac7db51563eac1f64d43c55dacf60c7
Rudy Jessop <rudyjessop@gmail.com>
2016-04-07 19:04:57 -0400

    final tests

--- tests/ActionableTest.php
+++ tests/ActionableTest.php
@@ -1,18 +1,30 @@
 <?php
 
+use CTL\SocialMapFavorites\Commands\FavAUserCommand;
+use CTL\SocialMapFavorites\Commands\UnFavAUserCommand;
+
 class ActionableTest extends TestCase{
 
   function test_favoritee_user_created(){
       $model = $this->makeUser();
 
-      $this->assertEquals(1,
-        $model->id
-      );
+      $this->assertEquals(1, $model->id);
   }
 
-    function test_it_stores_the_favoritee_id_from_user_model(){
+    function test_if_it_stores_the_favoritee_id(){
+      $user = new User;
 
+      $FavAUserCommand = new FavAUserCommand($user->userID, $user->userIDToFav);
 
+      $user->favUsers()->attach($user->userIDToFav);
 
+    }
+
+    function test_if_it_deletes_the_favoritee_id(){
+      $user = new User;
+
+      $UnFavAUserCommand = new unFavAUserCommand($user->userID, $user->userIDToUnFav);
+
+      $user->favUsers()->detach($user->userIDToUnFav);
     }
 }
